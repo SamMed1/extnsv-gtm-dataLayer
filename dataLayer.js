d0c7f68ac3a40d4fa394364b91cea1e2b1e7de46
Sam Medhurst <sammedhurst777@gmail.com>
2020-08-04 15:18:59 +0100

    Convert quantity value to an integer rather than a string. Add static variables for Dragon Home product page.

--- dataLayer.js
+++ dataLayer.js
@@ -11,25 +11,39 @@ document.addEventListener("DOMContentLoaded", function (event) {
 	// **  Define static variables to be used by specific pages.
 	// [1] Dragon Professional Individual page
 	const dragonProfessional = document.getElementById("dragon-professional");
-	// [2] Another page
-	const pageName = document.getElementById("############");
+	// [2] Dragon Home page
+	const dragonHome = document.getElementById("dragon-home");
 	// [3] Another page
 	const pageName2 = document.getElementById("############");
 
 	// [1]
-	if ( dragonProfessional ) {
+	if (dragonProfessional) {
 		// Define static variables.
 		var gtm_pageName = 'Dragon Professional Individual 15';
-		var gtm_brand = 'Dragon';
+		var gtm_brand    = 'Dragon';
 		var gtm_category = 'Speech Recognition';
-		var gtm_variant = 'Download';
+		var gtm_variant  = 'Download';
 
 		// Define variation pid's.
 		var pid_standard = '5065051500';
 		var pid_wireless = '5383602700';
-		var pid_upgrade = '5062971500';
-		var pid_spanish = '5228063700';
-		var pid_french = '5228064000';
+		var pid_upgrade  = '5062971500';
+		var pid_spanish  = '5228063700';
+		var pid_french   = '5228064000';
+	}
+
+	// [2]
+	if (dragonHome) {
+		// Define static variables.
+		var gtm_pageName = 'Dragon Home';
+		var gtm_brand    = 'Dragon';
+		var gtm_category = 'Speech Recognition';
+		var gtm_variant  = 'Download';
+
+		// Define variation pid's.
+		var pid_standard = '5217991100';
+		var pid_wireless = '5383602000';
+		var pid_french   = '5240834400';
 	}
 
 	// ** Call functions to generate our dynamic variables are calculated depending on user selection, eg price.
@@ -79,7 +93,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 	// [3]
 	// Functionality to detect quantity selection and define variable for use in dataLayer.
 	function checkQuantity() {
-		var gtm_quantity = document.getElementById('quantity').value;
+		let gtm_quantity_str = document.getElementById('quantity').value;
+		var gtm_quantity = parseInt(gtm_quantity_str);
 		return gtm_quantity;
 	}
 	checkQuantity();
@@ -115,11 +130,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
 		}
 
 		// Prices
-		if ( gtm_currencyCode == 'CAD' ) {
+		if (gtm_currencyCode == 'CAD') {
 			var bluetoothHeadsetCost = checkBox1.getAttribute("data-cad");
 			var powermicCost = checkBox2.getAttribute("data-cad");
-		}
-		else {
+		} else {
 			var bluetoothHeadsetCost = checkBox1.getAttribute("data-us");
 			var powermicCost = checkBox2.getAttribute("data-us");
 		}
@@ -139,7 +153,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 		var accessoriesAll = checkAccessories(); // Retrieve all of our accessory truthy / falsey variables, then ..
 		var bluetoothHeadsetCost = accessoriesAll[2]; // .. segment bluetooth headset cost variable ..
-		var powermicCost = accessoriesAll[3];  // .. segment powermic cost variable.
+		var powermicCost = accessoriesAll[3]; // .. segment powermic cost variable.
 
 		// [1]
 		var bluetoothHeadset = {
@@ -170,7 +184,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 			'category': gtm_category,
 			'variant': gtm_variant,
 			'quantity': gtm_quantity
-		}
+		};
 
 		return [product, bluetoothHeadset, powermic];
 	}
@@ -179,24 +193,24 @@ document.addEventListener("DOMContentLoaded", function (event) {
 	// ** Now define our action list, then call the relevant functions in order to pass our data to the dataLayer for viewing in GTM and GA. **
 
 	// Action list.
-	document.querySelector('.windows-text').addEventListener("click", gtmAddToCart); // Add to cart script.
+	document.querySelector('.bt-buynow').addEventListener("click", gtmAddToCart); // Trigger AddToCart script.
 
 	function gtmAddToCart() {
 
 		// *** Retrieve our dynamic variables from functions outside of block scope. ***
 		var gtm_currencyCode = checkCurrency(); // Retrieve currency code.
-		
+
 		var accessoriesAll = checkAccessories(); // Retrieve all of our accessory truthy / falsey variables, then ..
 		var accessoriesBluetoothHeadset = accessoriesAll[0]; // .. segment bluetooth headset variable ..
-		var accessoriesPowermic = accessoriesAll[1];  // .. segment powermic variable.
+		var accessoriesPowermic = accessoriesAll[1]; // .. segment powermic variable.
 
 		var productsAll = dataStructure(); // Retrieve our data structure, then ..
 		var productsPrimary = productsAll[0]; // .. segment primary product structure ..
 		var productsBluetoothHeadset = productsAll[1]; // .. segment bluetooth headset structure ..
 		var productsPowermic = productsAll[2]; // .. segment powermic structure.
 
-		// *** Action taken if bluetooth headset added.
-		if ( accessoriesPowermic && accessoriesBluetoothHeadset) {
+		// *** Action taken if both accessories added.
+		if (accessoriesPowermic && accessoriesBluetoothHeadset) {
 			dataLayer.push({
 				'event': 'addToCart',
 				'ecommerce': {
@@ -211,6 +225,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 				}
 			});
 		}
+		// *** Action taken if bluetooth headset added.
 		else if (accessoriesBluetoothHeadset) {
 			dataLayer.push({
 				'event': 'addToCart',
@@ -225,7 +240,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 				}
 			});
 		}
-		else if ( accessoriesPowermic ) {
+		// *** Action taken if powermic added.
+		else if (accessoriesPowermic) {
 			dataLayer.push({
 				'event': 'addToCart',
 				'ecommerce': {
@@ -239,6 +255,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 				}
 			});
 		}
+		// *** Action taken if no accessories added.
 		else {
 			dataLayer.push({
 				'event': 'addToCart',
