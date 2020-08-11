c7bc8e0d90be582204b9715ecf988782722b3c8e
Sam Medhurst <sammedhurst777@gmail.com>
2020-08-11 15:59:17 +0100

    Develop add to cart scipt, then move onto the accessory checkbox scripts. Need to the powermic 3 price band PID logic.

--- dataLayer.js
+++ dataLayer.js
@@ -12,9 +12,15 @@ document.addEventListener("DOMContentLoaded", function (event) {
 	// [1] Dragon Professional Individual page
 	const dragonProfessional = document.getElementById("dragon-professional");
 	// [2] Dragon Home page
-	const dragonHome = document.getElementById("dragon-home");
-	// [3] Another page
-	const pageName2 = document.getElementById("############");
+	const dragonHome         = document.getElementById("dragon-home");
+	// [3] Dragon Legal page
+	const dragonLegal        = document.getElementById("dragon-legal");
+	// [4] Dragon Bluetooth Headset
+	const bluetoothHeadset   = document.getElementById("bluetooth-headset");
+	// [5] Dragon Powermic 2
+	const powermic2          = document.getElementById("powermic2");
+	// [6] Dragon Powermic 3
+	const powermic3          = document.getElementById("powermic3");
 
 	// [1]
 	if (dragonProfessional) {
@@ -46,6 +52,60 @@ document.addEventListener("DOMContentLoaded", function (event) {
 		var pid_french   = '5240834400';
 	}
 
+	// [3]
+	if (dragonLegal) {
+		// Define static variables.
+		var gtm_pageName = 'Dragon Legal';
+		var gtm_brand    = 'Dragon';
+		var gtm_category = 'Speech Recognition';
+		var gtm_variant  = 'Download';
+
+		// Define variation pid's.
+		var pid_standard      = '5062973600';
+		var pid_wireless      = '5383602400';
+		var pid_upgrade       = '5063131500';
+		var pid_upgrade_legal = '5063131700';
+	}
+
+	// [4]
+	if (bluetoothHeadset) {
+		// Define static variables.
+		var gtm_pageName = 'Dragon Bluetooth Headset For Healthcare';
+		var gtm_brand    = 'Dragon';
+		var gtm_category = 'Speech Recognition';
+		var gtm_variant  = 'Download';
+
+		// Define variation pid's.
+		var pid_standard = '307076500';
+	}
+
+	// [5]
+	if (powermic2) {
+		// Define static variables.
+		var gtm_pageName = 'Dragon Powermic 2';
+		var gtm_brand    = 'Dragon';
+		var gtm_category = 'Speech Recognition';
+		var gtm_variant  = 'Download';
+
+		// Define variation pid's.
+		var pid_standard         = '5412502300'; // Variation: without scanner.
+		var pid_standard_scanner = '5412503000'; // Variation: with scanner.
+	}
+
+	// [6]
+	if (powermic3) {
+		// Define static variables.
+		var gtm_pageName = 'Dragon Powermic 3';
+		var gtm_brand    = 'Dragon';
+		var gtm_category = 'Speech Recognition';
+		var gtm_variant  = 'Download';
+
+		// Define variation pid's.
+		var pid_standard     = '5411962900'; // Variation: 9ft.
+		var pid_standard_3ft = '5411962700'; // Variation: 3ft.
+	}
+
+
 	// ** Call functions to generate our dynamic variables are calculated depending on user selection, eg price.
 	// [1] Generate variable for currency.
 	// [2] Generate variable for variation.
@@ -71,21 +131,54 @@ document.addEventListener("DOMContentLoaded", function (event) {
 	// [2]
 	// Functionality to detect variation selection and define variable for use in dataLayer.
 	function checkVariation() {
-		// Get the selected product lowercase value for use.
-		var variationRendered = document.getElementById("select2-variation-container").textContent;
-		var variationRenderedLower = variationRendered.toLowerCase();
+		// Check the variation containers being used before reading internal value as they slightly differ on each page in older site versions.
+		var variation = document.getElementById("select2-variation-container"); // Class used for the standard products and accessories with a variation selector.
+		var variationDragonHome = document.getElementById("select2-variation-dh-container"); // Dragon Home ID Variation
+		var variationDragonLegal = document.getElementById("select2-variation-dl-container"); // Dragon Legal ID Variation
+		var variationAccessory = document.getElementsByClassName("variation-grey");
 
-		if (variationRenderedLower == 'standard') {
+		// Check if elements exist, then get the selected product lowercase value for use.
+		if ( variation ) {
+			var variationRendered = document.getElementById("select2-variation-container").textContent;
+		}
+		else if ( variationDragonHome ) {
+			var variationRendered = document.getElementById("select2-variation-dh-container").textContent;
+		}
+		else if ( variationDragonLegal ) {
+			var variationRendered = document.getElementById("select2-variation-dl-container").textContent;
+		}
+		else if ( variationAccessory ) {
+			var variationRendered = document.getElementsByClassName("variation-grey")[0].textContent;
+		}
+
+		var variationRenderedLower = variationRendered.toLowerCase().replace(/\s+/g, ''); // Convert to lowercase string and strip out any empty spaces before using.
+
+		if (variationRenderedLower == 'standard'){
 			var gtm_id = pid_standard;
 		} else if (variationRenderedLower == 'wireless') {
 			var gtm_id = pid_wireless;
 		} else if (variationRenderedLower == 'upgrade') {
 			var gtm_id = pid_upgrade;
+		} else if (variationRenderedLower == 'upgradefromlegal12') {
+			var gtm_id = pid_upgrade_legal;
 		} else if (variationRenderedLower == 'spanish') {
 			var gtm_id = pid_spanish;
 		} else if (variationRenderedLower == 'french') {
 			var gtm_id = pid_french;
+		} else if (variationRenderedLower == 'bluetoothheadset') { // Accessory: Bluetooth headset single pid.
+			var gtm_id = pid_standard;
+		} else if (variationRenderedLower == 'withoutscanner') { // Accessory: Powermic 2 with scanner pid.
+			var gtm_id = pid_standard;
+		} else if (variationRenderedLower == 'withscanner') { // Accessory: Powermic 2 without scanner pid.
+			var gtm_id = pid_standard_scanner;
+		} else if (variationRenderedLower == 'powermic9ftcord') { // Accessory: Powermic 3 9ft.
+			var gtm_id = pid_standard;
+		} else if (variationRenderedLower == 'powermic3ftcord') { // Accessory: Powermic 3 3ft.
+			var gtm_id = pid_standard_3ft;
+		} else {
+			var gtm_id = 'outofstock';
 		}
+		 
 		return gtm_id;
 	}
 	checkVariation();
@@ -113,103 +206,171 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 		var gtm_currencyCode = checkCurrency(); // Pull in currency code.
 
-		// Checkbox 1
-		const checkBox1 = document.getElementById("check1");
-		if (checkBox1.checked) {
-			var addBluetoothHeadset = true;
-		} else {
-			var addBluetoothHeadset = false;
+		// Bluetooth Headset
+		const bhs = document.querySelector('.b-head-cb');
+		if ( bhs ) {
+			if (bhs.checked) {
+				var addBluetoothHeadset = true;
+			} else {
+				var addBluetoothHeadset = false;
+			}
 		}
 
-		// Checkbox 2
-		const checkBox2 = document.getElementById("check2");
-		if (checkBox2.checked) {
-			var addPowermic = true;
-		} else {
-			var addPowermic = false;
+		// USB Headset
+		const usb = document.querySelector('.usb-head-cb');
+		if ( usb ) {
+			if (usb.checked) {
+				var addUSBHeadset = true;
+			} else {
+				var addUSBHeadset = false;
+			}
+		}
+
+		// Powermic
+		const pm = document.querySelector('.p-mic-cb');
+		if ( pm ) {
+			if (pm.checked) {
+				var addPowermic = true;
+			} else {
+				var addPowermic = false;
+			}
 		}
 
 		// Prices
 		if (gtm_currencyCode == 'CAD') {
-			var bluetoothHeadsetCost = checkBox1.getAttribute("data-cad");
-			var powermicCost = checkBox2.getAttribute("data-cad");
+			if ( bhs ) {
+				var bluetoothHeadsetCost = bhs.getAttribute("data-cad");
+			}
+			if ( usb ) {
+				var usbHeadsetCost       = usb.getAttribute("data-cad");
+			}
+			if ( pm ) {
+				var powermicCost         = pm.getAttribute("data-cad");
+			}
 		} else {
-			var bluetoothHeadsetCost = checkBox1.getAttribute("data-us");
-			var powermicCost = checkBox2.getAttribute("data-us");
+			if ( bhs ) {
+				var bluetoothHeadsetCost = bhs.getAttribute("data-us");
+			}
+			if ( usb ) {
+				var usbHeadsetCost       = usb.getAttribute("data-us");
+			}
+			if ( pm ) {
+				var powermicCost         = pm.getAttribute("data-us");
+			}
 		}
 
-		return [addBluetoothHeadset, addPowermic, bluetoothHeadsetCost, powermicCost];
+		return [addBluetoothHeadset, addUSBHeadset, addPowermic, bluetoothHeadsetCost, usbHeadsetCost, powermicCost];
 	}
 
 	// ** Store the data structure in a function, written with the help of the page specific variables as defined above. **
 	// [1] Bluetooth headset
-	// [2] PowerMic 3
-	// [3] The primary product
+	// [2] USB headset
+	// [3] PowerMic 3
+	// [4] The primary product
 
 	function dataStructure() {
-		var gtm_id = checkVariation();
+		var gtm_id       = checkVariation();
 		var gtm_quantity = checkQuantity();
-		var gtm_price = checkPrice();
+		var gtm_price    = checkPrice();
 
-		var accessoriesAll = checkAccessories(); // Retrieve all of our accessory truthy / falsey variables, then ..
-		var bluetoothHeadsetCost = accessoriesAll[2]; // .. segment bluetooth headset cost variable ..
-		var powermicCost = accessoriesAll[3]; // .. segment powermic cost variable.
+		var accessoriesAll       = checkAccessories(); // Retrieve all of our accessory truthy / falsey variables, then ..
+		var bluetoothHeadsetCost = accessoriesAll[3]; // .. segment bluetooth headset cost variable ..
+		var usbHeadsetCost       = accessoriesAll[4]; // .. segment bluetooth headset cost variable ..
+		var powermicCost         = accessoriesAll[5]; // .. segment powermic cost variable.
 
 		// [1]
 		var bluetoothHeadset = {
-			id: '5089594000',
-			name: 'Dragon USB Headset',
-			price: bluetoothHeadsetCost,
-			brand: 'Dragon',
+			id:       '307076500',
+			name:     'Dragon Bluetooth Wireless Headset',
+			price:    bluetoothHeadsetCost,
+			brand:    'Dragon',
 			category: 'Headset',
 			quantity: 1 // The quantity will always be 1 when added to the basket.
 		};
 
 		// [2]
-		var powermic = {
-			id: '#########',
-			name: 'PowerMic',
-			price: powermicCost,
-			brand: 'Dragon',
+		var usbHeadset = {
+			id:       '5089594000',
+			name:     'Dragon USB Headset',
+			price:    usbHeadsetCost,
+			brand:    'Dragon',
 			category: 'Headset',
 			quantity: 1 // The quantity will always be 1 when added to the basket.
 		};
 
 		// [3]
+		var powermic = {
+			id:       '5056167100',
+			name:     'PowerMic',
+			price:    powermicCost,
+			brand:    'Dragon',
+			category: 'Headset',
+			quantity: 1 // The quantity will always be 1 when added to the basket.
+		};
+
+		// [4]
 		var product = {
-			'name': gtm_pageName,
-			'id': gtm_id,
-			'price': gtm_price,
-			'brand': gtm_brand,
+			'name':     gtm_pageName,
+			'id':       gtm_id,
+			'price':    gtm_price,
+			'brand':    gtm_brand,
 			'category': gtm_category,
-			'variant': gtm_variant,
+			'variant':  gtm_variant,
 			'quantity': gtm_quantity
 		};
 
-		return [product, bluetoothHeadset, powermic];
+		return [product, bluetoothHeadset, usbHeadset, powermic];
 	}
 	dataStructure();
 
 	// ** Now define our action list, then call the relevant functions in order to pass our data to the dataLayer for viewing in GTM and GA. **
 
-	// Action list.
-	document.querySelector('.bt-buynow').addEventListener("click", gtmAddToCart); // Trigger AddToCart script.
+	// ** Action list.
+	// [1] AddToCart script triggers.
+	// [2] Accessories Checkbox script triggers.
 
+	// [1]
+	document.querySelector('.bt-buynow').addEventListener("click", gtmAddToCart);
+	document.querySelector('.windows-text').addEventListener("click", gtmAddToCart); // TODO: Delete dummy actions.
+	document.querySelector('.fixed-size').addEventListener("click", gtmAddToCart); // TODO: Delete dummy actions.
+	
+	// [2]
+	const USBHeadsetCheckbox       = document.querySelector('.usb-head-cb');
+	const bluetoothHeadsetCheckbox = document.querySelector('.b-head-cb');
+	const powermicCheckbox         = document.querySelector('.p-mic-cb');
+
+	if ( USBHeadsetCheckbox ) {
+		USBHeadsetCheckbox.addEventListener("click", gtmUSBHeadsetCheck); // Trigger USBHeadsetCheck script.
+	}
+	if ( bluetoothHeadsetCheckbox ) {
+		bluetoothHeadsetCheckbox.addEventListener("click", gtmBluetoothHeadsetCheck); // Trigger BluetoothHeadsetCheck script.
+	}
+	if ( powermicCheckbox ) {
+		powermicCheckbox.addEventListener("click", gtmPowermicCheck); // Trigger PowermicCheck script.
+	}
+
+	// Function list.
+	// [1] gtmAddToCart() - This function handles the retrieval and dataLayer push of the combination of user product selection, at the time of calling.
+	// [2] checkboxPush(), gtmBluetoothHeadsetCheck(), gtmUSBHeadsetCheck(), gtmPowermicCheck() - These functions push an accessory object to the dataLayer depending on the checkbox selected.
+
+	// [1]
 	function gtmAddToCart() {
 
 		// *** Retrieve our dynamic variables from functions outside of block scope. ***
 		var gtm_currencyCode = checkCurrency(); // Retrieve currency code.
 
-		var accessoriesAll = checkAccessories(); // Retrieve all of our accessory truthy / falsey variables, then ..
+		var accessoriesAll              = checkAccessories(); // Retrieve all of our accessory truthy / falsey variables, then ..
 		var accessoriesBluetoothHeadset = accessoriesAll[0]; // .. segment bluetooth headset variable ..
-		var accessoriesPowermic = accessoriesAll[1]; // .. segment powermic variable.
+		var accessoriesUSBHeadset       = accessoriesAll[1]; // .. segment USB headset variable ..
+		var accessoriesPowermic         = accessoriesAll[2]; // .. segment powermic variable.
 
-		var productsAll = dataStructure(); // Retrieve our data structure, then ..
-		var productsPrimary = productsAll[0]; // .. segment primary product structure ..
+		var productsAll              = dataStructure(); // Retrieve our data structure, then ..
+		var productsPrimary          = productsAll[0]; // .. segment primary product structure ..
 		var productsBluetoothHeadset = productsAll[1]; // .. segment bluetooth headset structure ..
-		var productsPowermic = productsAll[2]; // .. segment powermic structure.
+		var productsUSBHeadset       = productsAll[2]; // .. segment USB headset structure ..
+		var productsPowermic         = productsAll[3]; // .. segment powermic structure.
 
-		// *** Action taken if both accessories added.
+		// *** Combination: Powermic & Bluetooth Headset.
 		if (accessoriesPowermic && accessoriesBluetoothHeadset) {
 			dataLayer.push({
 				'event': 'addToCart',
@@ -225,7 +386,42 @@ document.addEventListener("DOMContentLoaded", function (event) {
 				}
 			});
 		}
-		// *** Action taken if bluetooth headset added.
+
+		// *** Combination: USB & Bluetooth Headset.
+		else if (accessoriesUSBHeadset && accessoriesBluetoothHeadset) {
+			dataLayer.push({
+				'event': 'addToCart',
+				'ecommerce': {
+					'currencyCode': gtm_currencyCode,
+					'add': {
+						'products': [
+							productsPrimary,
+							productsBluetoothHeadset,
+							productsUSBHeadset
+						]
+					}
+				}
+			});
+		}
+
+		// *** Combination: USB & Powermic.
+		else if (accessoriesUSBHeadset && accessoriesPowermic) {
+			dataLayer.push({
+				'event': 'addToCart',
+				'ecommerce': {
+					'currencyCode': gtm_currencyCode,
+					'add': {
+						'products': [
+							productsPrimary,
+							productsPowermic,
+							productsUSBHeadset
+						]
+					}
+				}
+			});
+		}
+
+		// *** Combination: Bluetooth Headset only.
 		else if (accessoriesBluetoothHeadset) {
 			dataLayer.push({
 				'event': 'addToCart',
@@ -240,7 +436,22 @@ document.addEventListener("DOMContentLoaded", function (event) {
 				}
 			});
 		}
-		// *** Action taken if powermic added.
+		// *** Combination: USB Headset only.
+		else if (accessoriesUSBHeadset) {
+			dataLayer.push({
+				'event': 'addToCart',
+				'ecommerce': {
+					'currencyCode': gtm_currencyCode,
+					'add': {
+						'products': [
+							productsPrimary,
+							productsUSBHeadset
+						]
+					}
+				}
+			});
+		}
+		// *** Combination: Powermic only.
 		else if (accessoriesPowermic) {
 			dataLayer.push({
 				'event': 'addToCart',
@@ -255,7 +466,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 				}
 			});
 		}
-		// *** Action taken if no accessories added.
+		// *** Combination: NO ACCESSORIES ADDED.
 		else {
 			dataLayer.push({
 				'event': 'addToCart',
@@ -270,5 +481,70 @@ document.addEventListener("DOMContentLoaded", function (event) {
 			});
 		}
 		console.log('dataLayer successfully pushed');
+	}
+
+	// [2]
+	/**
+		* @param {string} currency  The user selected currency at the time of calling the function.
+		* @param {Object} accessory The relevant accessory object as defined in the dataStructure function.
+	*/
+	function checkboxPush(currency, accessory) {
+		dataLayer.push({
+			'event': 'productClick',
+			'ecommerce': {
+				'currencyCode': currency,
+				'click': {
+					'actionField': {
+						'list': 'Accessories check box'
+					},
+					'products': [
+						accessory
+					]
+				}
+			}
+		});
+		console.log('dataLayer successfully pushed');
+	}
+	
+	// Function to pass the Bluetooth Headset data structure into the checkboxPush function.
+	function gtmBluetoothHeadsetCheck() {
+		// *** Retrieve our dynamic variables from functions outside of block scope. ***
+		var gtm_currencyCode         = checkCurrency(); // Retrieve currency code.
+		var productsAll              = dataStructure(); // Retrieve our data structure, then ..
+		var productsBluetoothHeadset = productsAll[1]; // .. segment bluetooth headset structure ..
+
+		// Only push to the data layer if the checkbox is checked.
+		const bluetoothHeadsetCheckbox = document.querySelector('.b-head-cb');
+		if (bluetoothHeadsetCheckbox.checked) {
+			checkboxPush(gtm_currencyCode, productsBluetoothHeadset);
+		}
+	}
+
+	// Function to pass the USB Headset data structure into the checkboxPush function.
+	function gtmUSBHeadsetCheck() {
+		// *** Retrieve our dynamic variables from functions outside of block scope. ***
+		var gtm_currencyCode   = checkCurrency(); // Retrieve currency code.
+		var productsAll        = dataStructure(); // Retrieve our data structure, then ..
+		var productsUSBHeadset = productsAll[2]; // .. segment USB headset structure ..
+
+		// Only push to the data layer if the checkbox is checked.
+		const USBHeadsetCheckbox  = document.querySelector('.usb-head-cb');
+		if (USBHeadsetCheckbox.checked) {
+			checkboxPush(gtm_currencyCode, productsUSBHeadset);
+		}
+	}
+
+	// Function to pass the Powermic data structure into the checkboxPush function.
+	function gtmPowermicCheck() {
+		// *** Retrieve our dynamic variables from functions outside of block scope. ***
+		var gtm_currencyCode   = checkCurrency(); // Retrieve currency code.
+		var productsAll        = dataStructure(); // Retrieve our data structure, then ..
+		var productsPowermic   = productsAll[3]; // .. segment bluetooth headset structure ..
+
+		// Only push to the data layer if the checkbox is checked.
+		const powermicCheckbox = document.querySelector('.p-mic-cb');
+		if (powermicCheckbox.checked) {
+			checkboxPush(gtm_currencyCode, productsPowermic);
+		}
 	}
 });
